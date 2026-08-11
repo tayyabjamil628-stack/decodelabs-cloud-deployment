2508acc70142f760e1687adf29d541df3130aa2f
Malik Tayyab Jamil <tayyabjamil628@gmail.com>
2026-08-11 11:05:19 +0500

    fix: make Vite assets compatible with all deployment targets

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/decodelabs-cloud-deployment/',
+    base: './',
 
     plugins: [react(), tailwindcss()],
 
